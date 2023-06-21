55c944c5ec134329c0115e79af2b85e57e4aa4f4
d.ziziko <d.ziziko@syberry.com>
2023-06-21 17:05:08 +0300

    647580: Add “Sorting data” feature

--- TheDataPOC/Domain/Models/Pedestrian.cs
+++ TheDataPOC/Domain/Models/Pedestrian.cs
@@ -1,19 +1,27 @@
-﻿namespace Domain.Models
+﻿using Sieve.Attributes;
+
+namespace Domain.Models
 {
 	public class Pedestrian
 	{
         public Guid Id { get; set; }
 
+        [Sieve(CanSort = true)]
         public DateTime? Date { get; set; }
 
+        [Sieve(CanSort = true)]
         public int? SeventhAndParkCampus { get; set; }
 
+        [Sieve(CanSort = true)]
         public int? BlineConventionCentre { get; set; }
 
+        [Sieve(CanSort = true)]
         public int? JordanAndSeventh { get; set; }
 
+        [Sieve(CanSort = true)]
         public int? NCollegeAndRailRoad { get; set; }
 
+        [Sieve(CanSort = true)]
         public int? SWalnutAndWylie { get; set; }
     }
 }
