66cb59fa7a3f957de1d6fa604dc6b20a8954e213
Egor <e.tomashov@syberry.com>
2023-06-22 16:38:03 +0300

    665116: Fix defect with seeder

--- TheDataPOC/Infrastructure/InfrastructureConfiguration.cs
+++ TheDataPOC/Infrastructure/InfrastructureConfiguration.cs
@@ -1,14 +1,11 @@
 ﻿namespace Infrastructure
 {
     using Database;
-    
-    using Infrastructure.Seeders;
-    
-    using Microsoft.AspNetCore.Identity;
+
     using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
-    
+
     using UnitOfWork;
 
     public static class InfrastructureConfiguration
@@ -32,21 +29,7 @@
 
             var serviceProvider = services.BuildServiceProvider();
 
-            InitializeUserRoles(serviceProvider);
-
             return services;
-        }
-
-        private static void InitializeUserRoles(IServiceProvider serviceProvider)
-        {
-            using (var scope = serviceProvider.CreateScope())
-            {
-                var unitOfWork = scope.ServiceProvider.GetRequiredService<IUnitOfWork>();
-
-                var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-
-                UserRolesSeeder.Initialize(unitOfWork, roleManager).GetAwaiter().GetResult();
-            }
         }
     }
 }
